8632f340e9fa200235336936b00a0978f4b4fc75
guptam1989 <guptam@nitj.ac.in>
2026-09-08 15:37:53 +0530

    Update conference contact numbers

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -50,7 +50,7 @@ function Contact() {
             {
               icon: Phone,
               title: "Phone",
-              lines: ["+919988190036", "+919882774691"],
+              lines: ["+9196797000950", "+918077878475", "+918637511985"],
             },
           ].map((c) => (
             <div key={c.title} className="card-elevated p-6">
